b7781a98a5c6855629d5c6583f4fbf032aea14a3
erikaArango <enfermeraerikaarango@gmail.com>
2020-11-16 10:44:50 -0300

    primer pull luzciel

--- src/data.js
+++ src/data.js
@@ -4,6 +4,26 @@ export const example = () => {
   return 'example';
 };
 
+
 export const anotherExample = () => {
   return 'OMG';
 };
+
+
+fetch('https://luzciel.github.io/SCL015-data-lovers/src/data/pokemon/pokemon.json')
+.then(response => response.json() )
+.then(data =>git));
+
+var namesPokemon = data.pokemon.name
+
+const alphabeticalOrder (name){
+  //orden ascendente
+namesPokemon.sort((a,b){return a - b})
+//orden descentente
+namesPokemon.sort((a,b){return b - a})
+console.log(namesPokemon)
+}
+
+function organizeInfo(data) {
+  data.data.forEach(alphabeticalOrder);
+}
