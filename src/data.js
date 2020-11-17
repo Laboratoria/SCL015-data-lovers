3d38082950d2e967fcdbba6e75315a2262f09da0
luzciel <luzcielmontesinos@gmail.com>
2020-11-17 14:06:46 -0300

    responsive celular

--- src/data.js
+++ src/data.js
@@ -1,10 +1,42 @@
+import data  from './data/pokemon.js'
+
+const  pokemon = data.pokemon; 
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
 
+let ordenaz = document.getElementById("clickme");
+ordenaz.addEventListener("click", function (a, b) {
+    if(a.name > b.name){
+        return 1;
+    }
+
+    if (a.name < b.name) {
+        return -1
+    }
+
+    return 0;
+
+});
+
+
+
+// pokemon.sort(function(a, b) {
+//   if(a.name > b.name){
+//       return 1;
+//   }
+
+//   if (a.name < b.name) {
+//       return -1
+//   }
+
+//   return 0;
+// });
+
+// console.log(pokemon);*/
