861a29abe0a32db35e783dc762bb547d0d248ab2
luzciel <luzcielmontesinos@gmail.com>
2020-11-18 00:56:33 -0300

    ordenar por alfabeto

--- src/data.js
+++ src/data.js
@@ -1,42 +1,115 @@
-import data  from './data/pokemon.js'
+//import data  from './data/pokemon/pokemon.js' // importar la data del data.js
 
-const  pokemon = data.pokemon; 
-// estas funciones son de ejemplo
 
-// export const example = () => {
-//   return 'example';
-// };
+export const orderAZ = (arrayPoke) => { 
+  arrayPoke.sort(function(a, b) {
+      if(a.name > b.name){
+          return 1;
+      }
+  
+      if (a.name < b.name) {
+          return -1
+      }
+      });
+  }
 
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
-
-let ordenaz = document.getElementById("clickme");
-ordenaz.addEventListener("click", function (a, b) {
-    if(a.name > b.name){
-        return 1;
+export const orderZA = (arrayPoke) => {    
+    arrayPoke.sort(function(a, b) {
+        if(a.name < b.name){
+            return 1;
+        }
+    
+        if (a.name > b.name) {
+            return -1
+        }
+        });
     }
 
-    if (a.name < b.name) {
-        return -1
-    }
+  export default {orderAZ, orderZA };
 
-    return 0;
+// const funciones = () => {
+//   let pokemon = data.pokemon;
+//   pokemon.sort(function(a, b) {
+//     if(a.name > b.name){
+//         return 1;
+//     }
 
-});
+//     if (a.name < b.name) {
+//         return -1
+//     }
+//     });
+    
+//   let sortdata = pokemon.sort() 
+//   console.log (sortdata)
+//   for (let i=0; i<sortdata.length; i++){
+// }
+// }
+
+// const validator = { funciones }
+// export default validator;
 
 
 
-// pokemon.sort(function(a, b) {
-//   if(a.name > b.name){
-//       return 1;
-//   }
 
-//   if (a.name < b.name) {
-//       return -1
-//   }
 
-//   return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import data  from './data/pokemon.js'
+
+// const  pokemon = data.pokemon; 
+// // estas funciones son de ejemplo
+
+// // export const example = () => {
+// //   return 'example';
+// // };
+
+// // export const anotherExample = () => {
+// //   return 'OMG';
+// // };
+
+// let ordenaz = document.getElementById("clickme");
+// ordenaz.addEventListener("click", function (a, b) {
+//     if(a.name > b.name){
+//         return 1;
+//     }
+
+//     if (a.name < b.name) {
+//         return -1
+//     }
+
+//     return 0;
+
 // });
 
-// console.log(pokemon);*/
+
+
+// // pokemon.sort(function(a, b) {
+// //   if(a.name > b.name){
+// //       return 1;
+// //   }
+
+// //   if (a.name < b.name) {
+// //       return -1
+// //   }
+
+// //   return 0;
+// // });
+
+// // console.log(pokemon);*/
--- src/main.js
+++ src/main.js
@@ -1,49 +1,93 @@
-import { example } from './data.js';
+//import { example } from './data.js';
+// import data  from './data/pokemon.js'
+import logic from './data.js'
 
-//import data from './data/pokemon/pokemon.js';
 
+//let dataInfo
+let listPokemon= document.getElementById("dataList"); // seccion HTML para las cards
+const orderPoke = document.getElementById("orderPokemon")
 
-let dataInfo
-
-// funcion para crear e Imprimir un elemento en HTML. Un <template> es un elemento que se utiliza 
-//para declarar fragmentos de HTML que se pueden utilizar en scripts.
+// Imprimir un elemento en HTML. 
 const htmlToElements= (html) => {
   let stencil = document.createElement('template');
   stencil.innerHTML = html; // .innerHTML devuelve o establece la sintaxis HTML describiendo los descendientes del elemento.
-  return stencil.content.firstChild; //Nodo.firstChild = propiedad que devuelve el primer hijo del nodo en el árbol
+  return stencil.content.firstChild; //Nodo.firstChild = devuelve el primer hijo del nodo
 }
-
 
 fetch('https://luzciel.github.io/SCL015-data-lovers/src/data/pokemon/pokemon.json')
   .then(response => response.json() )
-  .then(data => {
-  
+  .then(data => { 
+    const dataPokemon = data.pokemon; // data pokemon del json
+    printData(dataPokemon); 
 
-  dataInfo = data.pokemon; // variable que contiene toda la data pokemon del json
+    orderPoke.addEventListener("change", () => { 
+      console.log("hola", orderPoke.value);
+      sortByAlphabet(dataPokemon, orderPoke) 
+      printData(dataPokemon) }
+  )})
+  .catch(function(error) {
+      return ('Hubo un problema con la petición Fetch:' + error.message);
+  });
+
+    const printData = (dataInfo) => {
+      listPokemon.innerHTML ="";
+      console.log("data", dataInfo)
+      for (let i=0; i<dataInfo.length; i++){
+        let card = htmlToElements(`<div class ="all-card">
+        <img src='${dataInfo[i].img}'/>
+        <p>'${dataInfo[i].name}'</p></div>`);
+        listPokemon.appendChild(card);
+      } 
+    }
+
+    const sortByAlphabet = (dataInfo, valor) => {
+        const valueOrder = valor.value;
+        console.log("entro en IF")
+        if(valueOrder === "1"){ 
+          console.log("entro en 1")
+          logic.orderAZ(dataInfo)
+          console.log(dataInfo);
+        }
+        if(valueOrder === "2"){
+          console.log("entro en 2")
+          logic.orderZA(dataInfo)
+        }
+
+
+        }
+
+        
+      // dataInfo = data.pokemon; // variable que contiene toda la data pokemon del json
+      // dataOrden.ordenar(dataInfo)
+      // //console.log(dataInfo[0].name); 
+      // const listPokemon = document.getElementById("dataList");
+      // for (let i=0; i<dataInfo.length; i++){
+      //   let card = htmlToElements(`<div class ="all-card">
+      //   <img src='${dataInfo[i].img}'/>
+      //   <p>'${dataInfo[i].name}'</p></div>`);
+      //   listPokemon.appendChild(card);
+      // } 
+
+
+
+      // dataInfo = data.pokemon; // variable que contiene toda la data pokemon del json
   //console.log(dataInfo[0].name);
-  //let tarjetaPoke = ""; 
-  let listPokemon= document.getElementById("dataList"); // seccion en HTML donde se introduciran las cards de los pokemones
-  for(let i= 0; i < dataInfo.length; ++i) {
-    //console.log(dataInfo[i].img)
-    let card = htmlToElements(`<div class ="all-card">
-    <img src='${dataInfo[i].img}'/>
-    <p>'${dataInfo[i].name}'</p></div>`);
-      //console.log(card);
-      listPokemon.appendChild(card);
-  }
-  //console.log(dataInfo.name.sort())
+    // for(let i= 0; i < dataInfo.length; ++i) {
+    // //console.log(dataInfo[i].img)
+    // let card = htmlToElements(`<div class ="all-card">
+    // <img src='${dataInfo[i].img}'/>
+    // <p>'${dataInfo[i].name}'</p></div>`);
+    //   //console.log(card);
+    //   listPokemon.appendChild(card);
+    // }
+
+//console.log(dataInfo.name.sort())
 
 
   // const orderPoke = document.getElementById("orderPokemon").value; 
   // const sortByAlphabet = () => {
      
   // }
-
-
-  })
-
-
-
 
 
 // fetch('https://luzciel.github.io/SCL015-data-lovers/src/data/pokemon/pokemon.json')
