40a0f5a84f2fbdde1c13ec5d8f3c3673536d89d4
luzciel <luzcielmontesinos@gmail.com>
2020-11-19 09:55:01 -0300

    Unitest

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,61 @@
-import { example, anotherExample } from '../src/data.js';
+//import { example, anotherExample } from '../src/data.js';
+import logic from '../src/data.js'
 
+// const examploData = [ {
+//   "name": "bulbasaur", "type": [
+//     "grass",
+//     "poison"
+//   ],
+//   },
+//   { 
+//   "name": "pikachu",
+//   "type": [
+//     "electric"
+//   ],
+//   },
+//   {
+//   "name": "raichu",
+//   "type": [
+//     "electric"
+//   ],
+//   }
+// ]
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('logic', () => {
+  it('debería ser un objeto', () => {
+    expect(typeof logic).toBe('object');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  describe('logic.orderAZ', () => {
+    it('debería ser una función', () => {
+      expect(typeof logic.orderAZ).toBe('function');
+    });
+  });
+    describe('logic.orderZA', () => {
+      it('debería ser una función', () => {
+        expect(typeof logic.orderAZ).toBe('function');
+        console.log(typeof logic.orderAZ)
+      });
   });
 });
 
+// describe('example', () => {
+//   skip.it('is a function', () => {
+//     expect(typeof example).toBe('function');
+//   });
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+//   skip.it('returns `example`', () => {
+//     expect(example()).toBe('example');
+//   });
+// });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+
+// skip.describe('anotherExample', () => {
+//   it('is a function', () => {
+//     expect(typeof anotherExample).toBe('function');
+//   });
+
+//   skip.it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
