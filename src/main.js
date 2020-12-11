6efc0436d7e8557facc41c23af8108adb5d845a2
Llaira <llairareyesd@gmail.com>
2020-12-11 12:29:08 -0600

    Modificamos HTML, creamos section y divs. Creamos el botón y dimos función al Botón

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
-import { example } from './data.js';
- import data from './data/lol/lol.js';
+//import { example } from './data.js';
+ //import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-
-const botonEntrar=document.getElementById("Entrar")
+const botonEntrar=document.getElementById("BotonEntrar")
 botonEntrar.addEventListener("click", entrar);
-//otorga un evento click a ingresar
-
-  function entrar() {
-   document.getElementById("Entrar").style.display="none";
+  
+function entrar() {
+   //document.getElementById("Entrar").style.display="none";
    document.getElementById("Inicio").style.display="none";
    document.getElementById("Tablero").style.display= "block";
-    //escondo y muestro pagina
+    
   }
 
 console.log(example, data);
