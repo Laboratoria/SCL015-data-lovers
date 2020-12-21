6ff2d6297355788f7d22a9e4c2a960564c758b7d
JavieraTM <javieratm13@gmail.com>
2020-12-21 08:36:15 -0300

    Cambios Index.html y style.css landing page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+// fetch('https://raw.githubusercontent.com/JavieraTM/SCL015-data-lovers/master/src/data/rickandmorty/rickandmorty.json')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const dataRickandMorty = data.RickandMorty;
+//     console.log(dataRickandMorty);
+//   })
