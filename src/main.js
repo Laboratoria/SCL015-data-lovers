a5cc2d4865416e57899ca35b0262b22677b8c482
Nico17munoz <nico.munoz11@gmail.com>
2020-12-21 09:33:58 -0300

    primeros comentarios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
 
 console.log(example, data);
+
+
+//llamo e invoco las funciones 
+//DOM - tomo los valores 
+//creacion de documento
