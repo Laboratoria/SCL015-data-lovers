0339bfd97564eef0a28c1f2e8830aadd0115c174
Llaira <llairareyesd@gmail.com>
2020-12-23 12:07:57 -0600

    Tablero, colores y personajes en web listos

--- src/data.js
+++ src/data.js
@@ -3,7 +3,21 @@
 //export const example = () => {
   //return 'example';
 //};
-
+import data from './data/lol/lol.js';
 //export const anotherExample = () => {
   //return 'OMG';
 //};
+export const ordenar = () => {
+  let opcionSelect = document.getElementById("sltOrden").value
+        console.log(opcionSelect);
+
+  let arrayDataLol = Object.values(data.data); 
+
+       
+       let datosOrdenados = arrayDataLol.sort((el1,el2) =>{
+         return (el1.name > el2.name) ? - 1 : 1
+      
+       })
+        console.log(datosOrdenados);
+  }
+     
--- src/main.js
+++ src/main.js
@@ -1,26 +1,35 @@
 //import { } from './data.js';
 //import { example } from './data.js';
+//import data from './data/lol/lol.js';
+//let dataLol = data.data;
+import { ordenar } from './data.js';
 import data from './data/lol/lol.js';
-let dataLol = data.data;
+
+
 let arrayDataLol = Object.values(data.data); 
 let containerData = document.getElementById("containerData")
 const botonEntrar = document.getElementById("BotonEntrar")
+const selectAz = document.getElementById("sltOrden")
 botonEntrar.addEventListener("click", entrar);
- 
+
 function entrar() {
 
-  document.getElementById("Inicio").style.display="none";
-  document.getElementById("Tablero").style.display= "block";
+   document.getElementById("Inicio").style.display="none";
+   document.getElementById("Tablero").style.display= "block";
 
-  for (let i = 0 ; i < arrayDataLol.length; i++){
-   let names = arrayDataLol[i].id;
- //  let imagenes = arrayDataLol[i].splash;
-  // let tipo = arrayDataLol[i].tags;
-   //let info = arrayDataLol[i].info;
-  //console.log(names, imagenes);
-  containerData.innerHTML += names;
- }
- }
+   //let listaData = "";
+   var obj = document.getElementById("containerData");
+   for(let i = 0; i < arrayDataLol.length; i++){
+    // let data = arrayDataLol[i].id;
+    obj.innerHTML += `<div class="card">
+    <img class="splash" src="${arrayDataLol[i].splash}"><div class="char-name">` + arrayDataLol[i].name + `</div>
+  </div>`;
+ //return listaData;
+}
+
+}
+
+selectAz.addEventListener("change", ordenar);
 
 const botonVolver=document.getElementById("BotonVolver")
 botonVolver.addEventListener("click", volver);
@@ -29,3 +38,5 @@ function volver() {
   document.getElementById("Inicio").style.display="block";
   document.getElementById("Tablero").style.display= "none";
  }
+
+console.log(example, data);
