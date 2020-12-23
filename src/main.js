097518829cad3b08ed1018f1b4d5ba73152fc9e1
Nico17munoz <nico.munoz11@gmail.com>
2020-12-23 08:32:03 -0300

    inicio filtrar data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 
 import data from './data/pokemon/pokemon.js';
+//en esta función queremos que al presionar pokebola de agua, apareceran los de tipo agua
+const pokemons = data.pokemon;
 
+function filterWater(){
+// espacio que contiene los distintos tipos (los elementos)
+// let space = "";
+    for (let index=0;index<pokemons.length;index++){
+        let position= pokemons[index].type;
+        console.log (position);
+        console.log (typeof(position));
+        if (position.includes("fire")){
+            console.log("hola");
+        }
+          //  console.log(pokemons[index].name);
+          //console.log(position);
+        //}
+
+   // console.log(pokemons[index].type);
+    }
+    
+//console.log(pokemones[index].name);
+}
+filterWater(pokemons);
 
 console.log(data);
 
