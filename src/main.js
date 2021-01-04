a0f63c219adf6a81e1c4bd3d91e2b398edbabe8a
JavieraTM <javieratm13@gmail.com>
2021-01-04 11:39:46 -0300

    segunda pantalla

--- src/main.js
+++ src/main.js
@@ -3,9 +3,3 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
 
-// fetch('https://raw.githubusercontent.com/JavieraTM/SCL015-data-lovers/master/src/data/rickandmorty/rickandmorty.json')
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const dataRickandMorty = data.RickandMorty;
-//     console.log(dataRickandMorty);
-//   })
