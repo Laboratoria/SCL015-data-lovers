88a0eb84d79da227fb35bd39ca6ed9bbd98d3809
JavieraTM <javieratm13@gmail.com>
2021-01-06 01:05:43 -0300

    Lista de personajes, con su respestiva imagen y caracteristicas en tarjeta formato flip

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import data from './data/rickandmorty/rickandmorty.js';
 console.log(example, data);
 
 
-
 //FUNCION HIDDEN-----------------------------------
 document.getElementById("startButton")
   .addEventListener("click", function () {
@@ -27,10 +26,24 @@ function charactersList() {
   let list = ""
   //Incio el bucle
   for (let i = 0; i < rickandmorty.length; i++) {
-    list += `<img src="${rickandmorty[i].image}" alt="">` + rickandmorty[i].name + "," + rickandmorty[i].status + "," + rickandmorty[i].gender + "," + rickandmorty[i].species + "." + rickandmorty[i].origin.name + "," + "<br>";
+    list += `<div class= "cardContainer">
+    <div id="rickandmorty" class="cardContainer-inner">
+    <div class="frontCard">
+    <p id="nameChar">${rickandmorty[i].name}
+    <img id="photo" class="photo" src="${rickandmorty[i].image}"/>
+    </p>
+    </div>
+    <div class="backCard">
+    <p id="statusChar" >${rickandmorty[i].status}</p>
+    <p id="genderChar" >${rickandmorty[i].gender}</p>
+    <p id="speciesChar" >${rickandmorty[i].species}</p>
+    </div>
+    </div>
+    </div>`;
     // console.log(rickandmorty[i]);
 
   }
   return list;
 }
 document.getElementById("listCharacters").innerHTML = charactersList();
+
