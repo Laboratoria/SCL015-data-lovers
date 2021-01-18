c62b2ae9abf773acee783a3a191f6814d5cc00aa
JavieraTM <javieratm13@gmail.com>
2021-01-18 10:29:00 -0300

    Ordenar personajes en pantalla

--- src/data.js
+++ src/data.js
@@ -1,5 +1,3 @@
-// import data from './data/rickandmorty/rickandmorty.js';
-
 // //Función para ordenar A-Z
 
 // export const alphabetOrderAZ = (a, b) => {
@@ -27,3 +25,47 @@
 // }
 // data.results.sort(alphabetOrderZA);
 // console.log(2,data.results);
+
+const orderFunctions = {
+
+  orderAZ(printCharacters) {
+    let orderedResults = printCharacters.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      { 
+        return -1;
+      }
+    });
+    console.log(1, orderedResults);
+    return orderedResults;
+  },
+
+  orderZA(printCharacters) {
+    let orderedResults = printCharacters.sort(function (a, b) {
+      if (a.name < b.name){
+        return 1;
+      }
+      {
+        return -1;
+      }
+    });
+    console.log(2, orderedResults);
+    return orderedResults;
+  },
+
+  orderDefault(printCharacters) {
+    let orderedResults = printCharacters.sort(function (a, b) {
+      if (a.id > b.id) {
+        return 1;
+      }
+      {
+        return -1;
+      }
+    });
+    console.log(3, orderedResults);
+    return orderedResults;
+  },
+}
+
+export default orderFunctions
--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-// import { alphabetOrderAZ } from './data.js';
-// import { alphabetOrderZA } from './data.js';
+import orderFunctions from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
 // console.log(alphabetOrderAZ, data);
 
 
-//FUNCION HIDDEN-----------------------------------
+//---------FUNCION HIDDEN-----------------------------------
 document.getElementById("startButton")
   .addEventListener("click", function () {
     document.getElementById("welcomeScreen").hidden = true;
@@ -19,143 +18,76 @@ document.getElementById("returnButton")
   }, false);
 
 
-let orderByName = data.results;
+let printCharacters = data.results;
 let listOfCharacters = document.getElementById("listCharacters");
 
-//LLAMAR LISTA DE PERSONAJES
+//-------------LLAMAR LISTA DE PERSONAJES-----------------
 
 const charactersList = () => {
-  //Declaro una variable para llamar la data desde rickandmorty.js
-  const rickandmorty = data.results;
   //Declaro una variable para imprimir la lista de personajes
-  let list = ""
+  let list = "";
   //Incio el bucle
-  for (let i = 0; i < rickandmorty.length; i++) {
+  for (let i = 0; i < printCharacters.length; i++) {
     list += `<div class= "cardContainer">
-    <div id="rickandmorty" class="cardContainer-inner">
+    <div id="printCharacters" class="cardContainer-inner">
     <div class="frontCard">
-      <img id="photo" class="photo" src="${rickandmorty[i].image}"/>
-      <p id="nameChar" class="name-character">${rickandmorty[i].name}</p>
+      <img id="photo" class="photo" src="${printCharacters[i].image}"/>
+      <p id="nameChar" class="name-character">${printCharacters[i].name}</p>
     </div>
     <div class="backCard">
-    <p id="nameChar" class="nameStyle">${rickandmorty[i].name}</p>
+    <p id="nameChar" class="nameStyle">${printCharacters[i].name}</p>
       <div class="backCardProperty">
         <div class="propertyFlex">
           <p class="propertyStyle">Status:</p>
-          <p id="statusChar" class="cardText">${rickandmorty[i].status}</p><br>
+          <p id="statusChar" class="cardText">${printCharacters[i].status}</p><br>
         </div>
         <div class="propertyFlex">
           <p class="propertyStyle">Specie: </p>
-          <p id="specieChar" class="cardText">${rickandmorty[i].species}</p><br>
+          <p id="specieChar" class="cardText">${printCharacters[i].species}</p><br>
         </div>
         <div class="propertyFlex">
           <p class="propertyStyle">Gender: </p>
-          <p id="genderChar" class="cardText">${rickandmorty[i].gender}</p><br>
+          <p id="genderChar" class="cardText">${printCharacters[i].gender}</p><br>
         </div>
         <div class="propertyFlex">
           <p class="propertyStyle">Last known location: </p>
-          <p id="locationChar" class="cardText">${rickandmorty[i].location.name}</p><br>
+          <p id="locationChar" class="cardText">${printCharacters[i].location.name}</p><br>
         </div>
       </div>
     </div>
     </div>
     </div>`;
-    // console.log(rickandmorty[i]);
+    // console.log(printCharacters[i]);
   }
   return list;
 }
 document.getElementById("listCharacters").innerHTML = charactersList();
 
-// const alphabetOrderAZ = (a, b) => {
-//   if (a.name < b.name){
-//     return -1;
-//   }
-//   if (a.name > b.name){
-//     return 1;
-//   }
-//   return 0;
-// }
-// data.results.sort(alphabetOrderAZ);
-// console.log(1, data.results);
 
-// Función para ordenar Z-A
-// export const alphabetOrderZA = (a, b) => {
-//   if (a.name < b.name){
-//     return -1;
-//   }
-//   if (a.name > b.name){
-//     return 1;
-//   }
-//   return 0;
-// }
-// data.results.reverse(alphabetOrderZA);
-// console.log(2,data.results);
-
-const orderFunctions = {
-
-    orderAZ(orderByName) {
-      let orderedResults = orderByName.sort(function (a, b) {
-        if (a.name > b.name) {
-          return 1;
-        }
-        { 
-          return -1;
-        }
-      });
-      console.log(1, orderedResults);
-      return orderedResults;
-    },
-
-    orderZA(orderByName) {
-      let orderedResults = orderByName.sort(function (a, b) {
-        if (a.name < b.name){
-          return 1;
-        }
-        {
-          return -1;
-        }
-      });
-      console.log(2, orderedResults);
-      return orderedResults;
-    },
-
-    orderDefault(orderByName) {
-      let orderedResults = orderByName.sort(function (a, b) {
-        if (a.id > b.id) {
-          return 1;
-        }
-
-        {
-          return -1;
-        }
-      });
-      console.log(3, orderedResults);
-      return orderedResults;
-    },
-    
-}
-
-
+//------------------SELECT PARA ORDENAR----------------
 let select = document.getElementById("selectOrderValue");
 select.addEventListener("change", function () {
 
   if (select.value === "AZorder") {
     listOfCharacters.innerHTML = "";
-    let array = orderFunctions.orderAZ(orderByName);
-    orderByName = array;
-    return charactersList(orderByName);
+    let array = orderFunctions.orderAZ(printCharacters);
+    printCharacters = array;
+    charactersList(printCharacters);
+    document.getElementById("listCharacters").innerHTML = charactersList();
   }
 
   else if (select.value === "ZAorder") {
-    let array = orderFunctions.orderZA(orderByName);
-    orderByName = array;
-    return charactersList(orderByName);
+    let array = orderFunctions.orderZA(printCharacters);
+    printCharacters = array;
+    charactersList(printCharacters);
+    document.getElementById("listCharacters").innerHTML = charactersList();
   }
 
   else {
-    let array = orderFunctions.orderDefault(orderByName);
-    orderByName = array;
-    return charactersList(orderByName)
+    let array = orderFunctions.orderDefault(printCharacters);
+    printCharacters = array;
+    charactersList(printCharacters)
+    document.getElementById("listCharacters").innerHTML = charactersList();
   }
 
 });
