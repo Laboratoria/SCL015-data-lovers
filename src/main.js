3fd4fd2850b32dab5dbd20c78a220a8e7c2fb153
Nico17munoz <nico.munoz11@gmail.com>
2021-01-19 13:16:26 -0300

    botones

--- src/main.js
+++ src/main.js
@@ -3,16 +3,34 @@ import data from './data/pokemon/pokemon.js';
 
 const pokemons = data.pokemon;
 
-const spacePokeballs=document.getElementById('spacePokeballs');
 
-function ocultar() {
-document.getElementById('btnTypes').addEventListener("click",function () {
-    document.getElementById("btnInitial").hidden = true;
-    document.getElementById("spacePokeballs").hidden = false;
-    
-},false);
-}
+const pokemonTypes= ['fire','normal'];
+const spacePokeballs= document.getElementById('spacePokeballs');
+document.getElementById("btnTypes").addEventListener('click', ()=>{
+    document.getElementById("btnInitial").style.display= "none";
+    document.getElementById("spacePokeballs").style.display= "block";
+    const spaceFragment= document.createDocumentFragment();
+   console.log(pokemonTypes);
+   
+;    pokemonTypes.forEach(typeOfPokemons=>{
+        let smallPokeball= document.createElement('button');
+        console.log(smallPokeball);
+        smallPokeball.type= 'button';
+        smallPokeball.value= typeOfPokemons;
+        smallPokeball.textContent= typeOfPokemons;
+        smallPokeball.setAttribute('id',typeOfPokemons);
+        smallPokeball.setAttribute('class', "miniP");
+console.log(smallPokeball);
+        spaceFragment.appendChild(smallPokeball);
+        console.log(spaceFragment);
+    });
+   
+spacePokeballs.appendChild(spaceFragment);
+console.log(spacePokeballs);
+})
+
+
 
 //llamo e invoco las funciones 
 //DOM - tomo los valores 
-// creacion de documento html dinamico
+/*creacion de documento html dinamico*/
