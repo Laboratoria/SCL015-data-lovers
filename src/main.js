5109c931899d3329905fd536f4612fb80dd7fe30
Nico17munoz <nico.munoz11@gmail.com>
2021-02-09 09:08:09 -0300

    boton select

--- src/main.js
+++ src/main.js
@@ -14,32 +14,51 @@ const pokemons = data.pokemon;
 //  }
 //  console.log(print);
 //  document.getElementById("cardContainer").innerHTML+=print;
-
- let tazos= (pokemons.forEach
- `<div class="cards">
- <h4>${(pokemons[i].name).toUpperCase(i)}</h4> <
- img src= "${pokemons[i].img}">
- <p>${pokemons[i].type}</p>
-</div>`);  
+let tazo="";
+document.getElementById("allPokemon").addEventListener('click', ()=>{
+document.getElementById("btnInitial").style.display= "none";
+document.getElementById("cardContainer").style.display= "block"; 
+document.getElementById("spacePokeball").style.display= "none";
+let buttonTazo=`
+<select value= "select" id="selectOrder" class="selectOrderAZ">
+<option selected disabled>Ordena</option>
+<option value="order A-Z" id="orderAZ">Ordena A - Z</option>
+<option value="order Z-A" id="orderZA">Ordena Z - A</option>
+</select>
+`;
+  for(let i=0;i< pokemons.length;i++){
+    tazo += `
+    <div class="cards">${i}
+    <h4>${(pokemons[i].name).toUpperCase(i)}</h4>
+    <img src= "${pokemons[i].img}">
+    <p>${pokemons[i].type}</p></div>
+    `;  
+  }
+   document.getElementById("cardContainer").innerHTML+=buttonTazo+ tazo;
+});
+ 
+//   const targetTazo= ()=>{
+//     const divTarget= document.createElement("div");
+//     divTarget.setAttribute("CLASS","cards");
+//     const viewTarget=`
+//     <h2></h2>
+// <img>
+// <ul>
+//   <li>hola</li>
+//   <li>hola</li>
+//   <li>hola</li>
+//   <p>hola</p>
+// </ul>
+//     `
+//     divTarget.appendChild(viewTarget);
+//   };
  
 
-let print =tazos(pokemons);
-console.log(print);
-
-  //   spacePokeballs.innerHTML = '';
-  // document.getElementById("btnInitial").style.display= "none";
-  // document.getElementById("cardContainer").style.display= "block"; 
-  //   for(let i=0;i< pokemons.length;i++){
-  //       document.getElementById("cardContainer").innerHTML+= `<div class="cards">${i}
-  //       <h4>${(pokemons[i].name).toUpperCase(i)}</h4> <img src= "${pokemons[i].img}"> <p>${pokemons[i].type}</p></div>`;  
-  //   }
-  
-
 const pokemonTypes= ['psychic','ice','fairy','fighting','poison','grass','fire','normal','steel','rock','dark','ground','flying','water','bug','dragon','electric','ghost'];
-const spacePokeballs= document.getElementById('spacePokeballs');
+const spacePokeballs= document.getElementById('spacePokeball');
 document.getElementById("btnTypes").addEventListener('click', ()=>{
     document.getElementById("btnInitial").style.display= "none";
-    document.getElementById("spacePokeballs").style.display= "block";
+    document.getElementById("spacePokeball").style.display= "block";
        
        let tittleTwo= document.createElement('h2');
        tittleTwo.textContent= 'Poke Tipo';
@@ -65,10 +84,12 @@ document.getElementById("btnTypes").addEventListener('click', ()=>{
       backButton.setAttribute('id',"back");
       spacePokeballs.appendChild(backButton);
 
-      backButton.addEventListener('click',()=>{
+      backButton.addEventListener('click',() =>{
+        // window.history.back();
         spacePokeballs.innerHTML = '';
-        
-})});
+      });
+});
+
 // const back= document.createElement("button");
 // back.type="button;"
 // back.addEventListener('click',function() {
@@ -79,4 +100,5 @@ document.getElementById("btnTypes").addEventListener('click', ()=>{
 //     ,false);
 
 pokemons.sort(aZorder);
+
 console.log(1,pokemons);
